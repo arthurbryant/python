ef75302bd43d8a3b43472e6b4123324e94689942
arthurbryant <cst.feng@gmail.com>
2013-06-16 08:58:29 +0900

    Python read and write file

--- io/file.py
+++ io/file.py
@@ -2,24 +2,23 @@
 import sys
 
 try:
-	f = open("fine.py", "r")
-except IOError
-	pass
-"""
-content = f.read()
-print content 
-sys.stdin.flush()
+	f = open("find.py", "r")
+	for line in f:
+		print line;
+	f.close();
+except IOError as e:
+	print "IO error ({0}): {1}".format(e.errno, e.strerror)
 
-f.seek(0)
-if True:
-    lines = f.readlines()
-    for line in lines:
-        print line
-    f.close()
 try:
-    f = open("noexist", "w")
-    f.write("start")
-    f.write("end")
-finally:
-    f.close()
-	"""
+	f = open("data", "r+")
+	f.write("I can write to python file\n");
+	f.write("Yeah, it's great\n");
+	f.seek(0)
+	for line in f:
+		# print without a newline
+		sys.stdout.write(line)
+	f.close()
+except IOError as e:
+	print "IO error {0}: {1}".format(e.errno, e.strerror)
+except:
+	print "Unexcepted error:", sys.exc_info()[0]
