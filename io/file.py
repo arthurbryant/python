88ccb31c477b1986fdcd98b13cbbba7e1ad2b716
arthurbryant <cst.feng@gmail.com>
2013-06-16 09:08:15 +0900

    Refactoring file.py

--- io/file.py
+++ io/file.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 import sys
 
+# read file
 try:
 	f = open("find.py", "r")
-	for line in f:
-		print line;
-	f.close();
+	try:
+		for line in f:
+			print line;
+	finally:
+		f.close()
 except IOError as e:
 	print "IO error ({0}): {1}".format(e.errno, e.strerror)
 
+# write to file
 try:
-	f = open("data", "r+")
-	f.write("I can write to python file\n");
-	f.write("Yeah, it's great\n");
-	f.seek(0)
-	for line in f:
-		# print without a newline
-		sys.stdout.write(line)
-	f.close()
+	with open("data", "r+") as f:
+		f.write("I can write to python file\n");
+		f.write("Yeah, it's great\n");
+		f.seek(0)
+		for line in f:
+			# print without a newline
+			sys.stdout.write(line)
 except IOError as e:
 	print "IO error {0}: {1}".format(e.errno, e.strerror)
 except:
